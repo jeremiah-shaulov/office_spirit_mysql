7b780dac4c0f29eaa2caa611a89ee4f2dbff0d0c
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2026-07-11 18:20:29 +0300

    Fixed running unittests WITH_DOCKER

--- private/tests/from_readme.test.ts
+++ private/tests/from_readme.test.ts
@@ -1,13 +1,13 @@
 import {testWithDocker} from './test_with_docker.ts';
 import {storeExamplesToTmpFiles} from 'https://deno.land/x/tsa@v0.0.55/doc_test/mod.ts';
 
-const tests = new Array<() => Promise<void>>;
+const tests = new Array<(dsnStr: string) => Promise<void>>;
 for (const {exampleName, filename} of await storeExamplesToTmpFiles(import.meta.url))
-{	const func = async function()
-	{	await import(filename);
+{	const func = async function(dsnStr: string)
+	{	Deno.env.set('DSN', dsnStr);
+		await import(filename);
 	};
 	Object.defineProperty(func, 'name', {value: exampleName, writable: false});
 	tests.push(func);
 }
-Deno.env.set('DSN', Deno.env.get('TESTS_DSN') ?? '');
 testWithDocker(tests);
